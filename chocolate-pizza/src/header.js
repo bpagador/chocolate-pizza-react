49f01670f8a9bc14c536f475274c518122ed712e
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-27 19:36:01 -0700

    added filies

--- chocolate-pizza/src/header.js
+++ chocolate-pizza/src/header.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import LogoTop from './header/LogoTop.js'
+import LogoLink from './header/LogoLink'
 
 export default class header extends Component {
     render() {
         return (
             <div>
-                header
+                the hader
+                <LogoTop />
+                <LogoLink />
             </div>
         )
     }
---
+++ chocolate-pizza/src/header/LogoLink.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class logoLink extends Component {
+    render() {
+        return (
+            <div>
+                <img src='./fb-icon.png' alt=" "/>
+ 
+            </div>
+        )
+    }
+}
